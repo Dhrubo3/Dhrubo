2f3bdf0f0735f7ae62ff51e20f36e35bb06b6ae6
Dhrubo3 <faizul.d.h.r.u.b.o@gmail.com>
2026-06-03 18:47:11 +0600

    Add files via upload

---
+++ 3.6.26/main.js
@@ -0,0 +1,86 @@
+// let num1 = 20;
+// let num2 = 5;
+// let result = num1 - num2;
+
+// let n1 = 10;
+// let n2 = 20;
+// let result = n1 += n2;
+// console.log(result);
+
+// let tem = 60;
+// if(tem>40){console.log("too hot");
+// }
+// else{console.log("too cold");
+// }
+
+
+// let tem = 1;
+// if(tem>18){console.log("Adult");
+// }
+// else{console.log("child");
+// }
+
+// let pass = 123;
+// if(pass=1234){console.log("welcome");
+// }
+// else{console.log("wrong pass");
+// }
+
+// let userName = "dhrubo";
+// let pass = "1234"
+// if(userName === "dhrubo" && pass === "1234"){console.log("welcome");
+// }else{
+//     console.log("wrong pass");
+    
+// }
+
+// let mark=1000;
+// if(mark >=80 && mark <=100){console.log("Your Result is A+");
+// }
+// else if(mark >=70 && mark <=79){console.log("Your result is A");
+// }
+// else if(mark >=60 && mark <=69){console.log("Your result is A-");
+// }
+// else if(mark >=50 && mark <=59){console.log("Your result is b+");
+// }
+// else if(mark >=40 && mark <=49){console.log("Your result is b");
+// }
+// else if(mark >=33 && mark <=39){console.log("Your result is pass");
+// }else{console.log("YOU are done bro");
+// }
+
+// let number =30;
+// switch(true) {
+//   case (number >=80 && number <=100):
+//     console.log("Your result is A");
+//     break;
+//   case (number >=70 && number <=79):
+//     console.log("Your result is b");
+//     break;
+//     case (number >=60 && number <=69):
+//     console.log("Your result is c");
+//     break;
+//     case (number >=50 && number <=59):
+//     console.log("Your result is d");
+//     break;
+//     case (number >=0 && number <=49):
+//     console.log("Your result is f");
+//     break;
+//   default:
+//     // code block
+// }
+
+// let num = 3
+// if (num % 2 === 0){console.log(num, ": even");
+// }else{ console.log(num, ":odd");
+// }
+
+// let ful = `Khan ibna faijul`;
+// console.log(ful.length);
+
+// let tex1= "hello";
+// let tex2= "world";
+// let final = tex1.concat(" ",tex2);
+// console.log(final);
+
+let tex1 = "Hello World!"
