b013838b72c99d8c86be47d370bca3f58805fdee
Dhrubo3 <faizul.d.h.r.u.b.o@gmail.com>
2026-06-04 17:49:45 +0600

    Add files via upload

---
+++ 4.6.26/main.js
@@ -0,0 +1,74 @@
+// let x = .1;
+// let y = .2;
+// let sum = (x + y.toFixed(2));
+// console.log(sum);
+
+// function batch(){
+//     console.log("he he he");
+    
+// } batch();
+
+// function summation(a, b){
+//     return a + b;
+// }
+// let result = summation(5, 7);
+// let result1 = summation(10, 7);
+// let result2 = summation(20, 5);
+// console.log(result);
+// console.log(result1);
+// console.log(result2);
+
+// console.log();
+
+
+// function subtraction(c, d){
+//     return c - d;
+// }
+// let result3 = subtraction(5, 7);
+// let result4 = subtraction(10, 7);
+// let result5 = subtraction(20, 5);
+// console.log(result3);
+// console.log(result4);
+// console.log(result5);
+
+// console.log();
+
+// function multiplication(e, f){
+//     return e * f;
+// }
+// let result6 = multiplication(5, 7);
+// let result7 = multiplication(10, 7);
+// let result8 = multiplication(20, 5);
+// console.log(result6);
+// console.log(result7);
+// console.log(result8);
+
+// console.log();
+
+// function 	division(g, h){
+//     return g / h;
+// }
+// let result9 = division(5, 7);
+// let result10 = division(10, 7);
+// let result11 = division(20, 5);
+// console.log(result9);
+// console.log(result10);
+// console.log(result11);
+
+// console.log();
+
+// function modulus(i, j){
+//     return i % j;
+// }
+// let result12 = modulus(5, 7);
+// let result13 = modulus(10, 7);
+// let result14 = modulus(20, 5);
+// console.log(result12);
+// console.log(result13);
+// console.log(result14);
+
+// console.log();
+
+
+
+
