6609b2d681a92ea77e24d36897abe922163cb8d4
Dhrubo3 <faizul.d.h.r.u.b.o@gmail.com>
2026-06-06 18:07:57 +0600

    Add files via upload

---
+++ 6.6.26/main.js
@@ -0,0 +1,65 @@
+// for(let i=0; i<=10; i++){
+//     if(i % 2!== 0)
+//     console.log(`${i}.Dhrubo Khan`);
+    
+// }
+// console.log();
+
+// for(let i=0; i<=10; i++){
+//     if(i % 2=== 0)
+//     console.log(`${i}.Dhrubo Khan`);
+    
+// }
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+    
+// }
+// while(i<=10)
+
+//     for(let i=1; i<=10; i++){
+//         if(i===6 || i===3 || i===7){continue;}
+//         console.log(i);
+        
+//     }
+
+// const fruits = ["apple", "mango", "liche", "banana"];
+// for(const fruit of fruits){
+//     console.log(fruit);
+    
+// }
+
+// const numbers = [12, 30 ,40, 54, 60]
+// numbers.forEach(function(num)
+// {
+//     console.log(num);
+    
+// })
+
+// for(i=1000; i>=1; i--){
+//     console.log(i);
+    
+// }
+
+// let i = 0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+    
+// }
+
+let i = 0;
+while(i<=1000){
+    console.log(i);
+    i++;
+    
+}
+
+let r = 1000;
+while(r>=1){
+    console.log(r);
+    r--;
+    
+}
